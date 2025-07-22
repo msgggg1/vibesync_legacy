4cfc7a349ae88092098b238fac62af4972006438
Yeji Kim <yejikim.erica@gmail.com>
2025-07-22 14:51:39 +0900

    board/view RequestParam 수정

--- src/main/java/com/vibesync/board/controller/BoardController.java
+++ src/main/java/com/vibesync/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.vibesync.board.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.vibesync.board.domain.NoteDetailDTO;
 import com.vibesync.board.domain.NoteListDTO;
 import com.vibesync.board.service.NoteService;
+import com.vibesync.common.annotation.AuthenticatedUserPages;
 import com.vibesync.common.domain.ContentVO;
 import com.vibesync.common.domain.Criteria;
 import com.vibesync.common.domain.GenreVO;
 import com.vibesync.common.domain.PageDTO;
 import com.vibesync.common.service.ContentService;
 import com.vibesync.common.service.GenreService;
+import com.vibesync.security.domain.CustomUser;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/board/*")
+@AuthenticatedUserPages
 public class BoardController {
 	
 	@Autowired
@@ -54,13 +58,13 @@ public class BoardController {
 		return "board/list";
 	}
 
-	// 게시글 작성 (/vibesync/board/view)
+	// 게시글 상세보기 (/vibesync/board/view)
 	@GetMapping(value="/view")
-	public String view(Model model, Criteria criteria, @RequestParam("note_idx") int note_idx) {
+	public String view(Model model, Criteria criteria, @RequestParam("noteIdx") int noteIdx, @AuthenticationPrincipal CustomUser user) {
 		log.info("게시글 상세보기 페이지 요청");
 		
 		// 게시글 상세보기
-		NoteDetailDTO noteDetail = this.noteService.findNoteByNoteIdx(note_idx);
+		NoteDetailDTO noteDetail = this.noteService.findNoteByNoteIdx(noteIdx);
 		model.addAttribute("noteDetail", noteDetail);
 		
 		return "board/view";
